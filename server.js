0db0c7e1a79a99743b93dd9a0fab545ad336366f
m7mdm1-dotcom <m7md.m1@gmail.com>
2026-03-09 02:06:20 +0200

    fix port

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get("/team", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
